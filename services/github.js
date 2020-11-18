57798ada01c91bcf439afa5de0249dd29b02c0bd
Ayush Sinha <ayushsinha126@gmail.com>
2020-11-18 21:22:15 +0530

    Changed the results page and added the start command in scripts prop in package.json

--- services/github.js
+++ services/github.js
@@ -1,25 +1,28 @@
 const axios = require('axios');
 
 var getRepo = (req,res,searchQuery)=>{
-    axios.get(`https://api.github.com/search/repositories?q=${searchQuery}&per_page=10&
-    client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`).then((result)=>{
-        //console.log(result.data.items);
-        //return data;
+    axios
+      .get(
+        `https://api.github.com/search/repositories?q=${searchQuery}+in:name,description&per_page=10&sort=stars&order=desc&
+    client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`
+      )
+      .then((result) => {
         var main = [];
         //console.log(result.data.items);
-        result.data.items.forEach(function(repo){
-            var obj = {};
-            obj.title = repo.name,
-            obj.description = repo.description,
-            obj.ownerName = repo.owner.login,
-            obj.repoURL = repo.html_url
-            main.push(obj);
+        result.data.items.forEach(function (repo) {
+          var obj = {};
+          (obj.title = repo.name),
+            (obj.description = repo.description),
+            (obj.ownerName = repo.owner.login),
+            (obj.repoURL = repo.html_url);
+          main.push(obj);
         });
-        return res.render('search',{repos: main});
-    }).catch((e)=>{
+        return res.render("results", { repos: main });
+      })
+      .catch((e) => {
         console.log(`Error in fetching the data from the github: ${e}`);
         return e;
-    });
+      });
 }
 
 module.exports = {
